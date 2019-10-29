ed50e0e8a206557f95251cd79d88d25fdb72dc33
Patricia Zambrano <pzambrano@novopayment.com>
2019-10-29 01:18:14 -0500

    Fix name of the key in the json

--- Weather/Weather/API/Decodables/ResponseDecodable.swift
+++ Weather/Weather/API/Decodables/ResponseDecodable.swift
@@ -9,18 +9,18 @@
 import Foundation
 
 class ResponseDecodable: Decodable {
-    var weathers: [WeatherDecodable]?
+    var weather: [WeatherDecodable]?
     var cod: String?
     var message: String?
     
     private enum CodingKeys: String, CodingKey {
-        case weathers
+        case weather
         case cod
         case message
     }
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.weathers = try? container.decode([WeatherDecodable].self, forKey: .weathers)
+        self.weather = try? container.decode([WeatherDecodable].self, forKey: .weather)
         self.cod = try? container.decode(String.self, forKey: .cod)
         self.message = try? container.decode(String.self, forKey: .message)
     }
