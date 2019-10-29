935f4d66e3ffd4cde6aa3f205e054938b145b41f
Patricia Zambrano <pzambrano@novopayment.com>
2019-10-28 20:27:15 -0500

    Create extensions to make a generic alert

--- Weather/Weather/Resources/TextsApp.swift
+++ Weather/Weather/Resources/TextsApp.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+enum TextsApps: String {
+    
+    //MARK: constants
+    case baseURL = "Base Url"
+    case imageURL = "Image Url"
+    case png = ".png"
+    
+    //MARK: alerts
+    case cancel = "Cancel"
+    case accept = "Accept"
+}
