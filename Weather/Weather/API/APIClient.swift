358da138f95dbe15d8187da93c24ca25b2c95928
Patricia Zambrano <pzambrano@novopayment.com>
2019-10-28 21:53:27 -0500

    Create functions to obtain the jsons

--- Weather/Weather/API/APIClient.swift
+++ Weather/Weather/API/APIClient.swift
@@ -29,9 +29,11 @@ class APIClient {
         return false
     }
     
-    static func downloadPhoto(completion: @escaping(Data?)-> Void) {
+    static func downloadPhoto(icon: String, completion: @escaping(Data?)-> Void) {
         let imageUrl = Bundle.main.infoDictionary?[TextsApps.imageURL.rawValue] as! String
-        AF.request(imageUrl)
+        let urlString = imageUrl + icon + TextsApps.png.rawValue
+        
+        AF.request(urlString)
             .response { (defaultDataResponse) -> Void in
                 if let imageData = defaultDataResponse.data {
                     completion(imageData)
@@ -39,6 +41,54 @@ class APIClient {
                     completion(nil)
                 }
         }
+    }
+    
+    static func getWeatherByCity(cityName: String, completion: @escaping ([WeatherDecodable]?)->Void) {
+        AF.request(APIRouter.getWeatherByCityName(cityName: cityName) as URLRequestConvertible)
+            .logRequest()
+            .responseJSON { (response) in
+                    switch response.result {
+                    case .success:
+                        if let json = response.value,
+                            let data = response.data {
+                            print("Success JSON: \(json)")
+                            do {
+                                let response = try JSONDecoder().decode(ResponseDecodable.self, from: data)
+                                completion(response.weather)
+                            } catch {
+                                print("Error with Decodable: \(error)")
+                                completion(nil)
+                            }
+                        }
+                    case .failure(let error):
+                        print("Error \(error)")
+                        completion(nil)
+                    }
+            }
+    }
+    
+    static func getWeatherByCoordinates(lat: Int, lon: Int, completion: @escaping ([WeatherDecodable]?)->Void) {
+        AF.request(APIRouter.getWeatherByCoordinates(lat: lat, lon: lon) as URLRequestConvertible)
+            .logRequest()
+            .responseJSON { (response) in
+                    switch response.result {
+                    case .success:
+                        if let json = response.value,
+                            let data = response.data {
+                            print("Success JSON: \(json)")
+                            do {
+                                let response = try JSONDecoder().decode(ResponseDecodable.self, from: data)
+                                completion(response.weather)
+                            } catch {
+                                print("Error with Decodable: \(error)")
+                                completion(nil)
+                            }
+                        }
+                    case .failure(let error):
+                        print("Error \(error)")
+                        completion(nil)
+                    }
+            }
     }
 }
 
